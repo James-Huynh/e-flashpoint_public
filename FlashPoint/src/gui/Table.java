db11f60707ca33906f2d24c5cb77ced2696c4531
zaidyahya <zaidyahya@142.157.145.231>
2019-03-13 21:04:54 -0400

    testing left click waiting button --

--- FlashPoint/src/gui/Table.java
+++ FlashPoint/src/gui/Table.java
@@ -1654,9 +1654,26 @@ public class Table {
 						firefighterMenu.add(info);
 					}
 					
+					
+					JMenuItem waiting = new JMenuItem("waiting");
+			        waiting.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							
+							if(listening() == 1) {
+								refresh(clientManager.getUsersGameState());
+								launcher.repaint(false,myIndex == clientManager.getUsersGameState().getActiveFireFighterIndex());
+							}
+							
+							
+						}
+					});
+					
 			        popupMenu.add(poiMenu);
 			        popupMenu.addSeparator();
 			        popupMenu.add(firefighterMenu);
+			        popupMenu.addSeparator();
+			        popupMenu.add(waiting);
 			        
 			        popupMenu.show(component, x, y);		// very important
 				}
