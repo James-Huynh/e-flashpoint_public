ee2c0f215247f1dec93c764918021da007bce6f2
James Huynh <james.huynh@mail.mcgill.ca>
2019-03-21 15:49:43 -0400

    worked on Jpane color

--- FlashPoint/src/custom_panels/ChatBox.java
+++ FlashPoint/src/custom_panels/ChatBox.java
@@ -1,6 +1,5 @@
 package custom_panels;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Rectangle;
@@ -12,30 +11,38 @@ import java.awt.event.KeyEvent;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Document;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 
 public class ChatBox extends JPanel{
 	private JTextField textField;
 	private JScrollPane scrollPane;
 	private JPanel panel_main;
 	private JButton btn_sendMess;
-	
-	private final Rectangle rect_main = new Rectangle(0, 0, 400, 500);	// arbitrary
+
+	private final Rectangle rect_main = new Rectangle(0, 0, 300, 500);	// arbitrary
 	private final Rectangle rect_textArea = new Rectangle(0, 0, (int) rect_main.getWidth(), (int) rect_main.getHeight() - 30);
 	private final Rectangle rect_chat = new Rectangle(0, (int) rect_textArea.getHeight() , (int) rect_main.getWidth() - 50, (int) (rect_main.getHeight() - rect_textArea.getHeight()));
 	private final Rectangle rect_button = new Rectangle((int) rect_chat.getWidth(), (int) rect_textArea.getHeight(), (int) (rect_main.getWidth() -  rect_chat.getWidth()), (int)  (rect_main.getHeight() - rect_textArea.getHeight()));
-	private JTextArea textArea;
-	
-	
-	
+	private JTextPane textPane;
+
+	private DefaultStyledDocument document;
+	private StyleContext context = new StyleContext();
+	private Style style = context.addStyle("test", null);
+
 	public ChatBox() {
 		setLayout(null);
-		
+
 		panel_main = new JPanel();
 		panel_main.setBounds(rect_main);
 		panel_main.setLayout(null);
-		
+
 		textField = new JTextField();
 		textField.addKeyListener(new KeyAdapter() {
 			@Override
@@ -46,27 +53,28 @@ public class ChatBox extends JPanel{
 				}
 			}
 		});
+
+
+
+		document = new DefaultStyledDocument();
+		textPane = new JTextPane(document);
 		
-		textArea = new JTextArea();
-		textArea.setFont(new Font("Open Sans", Font.BOLD, 18));
-		textArea.setText("lololol");
-		textArea.setBackground(Color.YELLOW);
-		textArea.setLineWrap(true);
-		textArea.setEnabled(false);
-		textArea.setBounds(rect_textArea);
+		textPane.setFont(new Font("Open Sans", Font.BOLD, 18));
+		textPane.setText("lololol");
+		textPane.setEnabled(false);
+		textPane.setBounds(rect_textArea);
 		
-		
-		scrollPane = new JScrollPane(textArea);
-		
+
+
+		scrollPane = new JScrollPane(textPane);
+
 		panel_main.add(scrollPane);
-		scrollPane.setBounds(new Rectangle(0, 0, 400, 470));
-	//	scrollPane.setLayout(null);
-		
-		
+		scrollPane.setBounds(rect_textArea);
+
 		textField.setBounds(rect_chat);
 		panel_main.add(textField);
 		textField.setColumns(10);
-		
+
 		btn_sendMess = new JButton("New button");
 		btn_sendMess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -76,23 +84,29 @@ public class ChatBox extends JPanel{
 		btn_sendMess.setBounds(rect_button);
 		panel_main.add(btn_sendMess);
 	}
-	
+
 
 	private boolean isEnterKey(KeyEvent e) {
 		return (e.getKeyCode() == KeyEvent.VK_ENTER);
 	}
-	
+
 
 	private void displayMessage() {
-		String currText = textArea.getText();
-		String newText = currText + "\n" + textField.getText();
-		
-		textArea.setText(newText);
-		
+		String currText = textPane.getText();
+		String newText = "\n" + textField.getText();
+
+		StyleConstants.setForeground(style, Color.red);
+		try {
+			document.insertString(document.getLength(), newText, style);
+		} catch(BadLocationException exc) {
+			exc.printStackTrace();
+		}
+
+
 		textField.setText("");
 	}
-	
-	
+
+
 	public JTextField getTextField() {
 		return textField;
 	}
